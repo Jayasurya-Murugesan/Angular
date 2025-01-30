088c99582afce160ec0afd09d6f9a4b3046b5494
Jayasurya B M <inboxofsurya@gmail.com>
2025-01-30 08:28:54 +0530

    tutorial 1- 8

--- angular-ekart/src/app/app.component.ts
+++ angular-ekart/src/app/app.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
+  selector: 'app-root', //wherever we mention this, html of this file will be rendered
+  templateUrl: './app.component.html', //or template property
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
